f61dc2e0ff3fbcd50c4256b75bd1e2494d3ea863
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2020-12-09 17:16:47 +0000

    veccore: fix CI warnings

--- Formula/veccore.rb
+++ Formula/veccore.rb
@@ -8,9 +8,8 @@ class Veccore < Formula
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install" 
   end
 
   test do
