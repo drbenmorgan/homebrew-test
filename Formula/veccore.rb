32f04c009879ec9291374e7aabaddaafb960d849
Ben Morgan <1102642+drbenmorgan@users.noreply.github.com>
2020-12-09 17:35:56 +0000

    veccore: add 0.6.0 bottle.

--- Formula/veccore.rb
+++ Formula/veccore.rb
@@ -5,6 +5,13 @@ class Veccore < Formula
   sha256 "db404d745906efec2a76175995e847af9174df5a8da1e5ccdb241c773d7c8df9"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/drbenmorgan/homebrew-test/releases/download/veccore-0.6.0"
+    cellar :any_skip_relocation
+    sha256 "80c3a2009d8f6c28fda66505fe68ebfc62dbf3c24a10c4ccc01b842b58efeb96" => :catalina
+    sha256 "b0045ca63b53b6f7292f75e8493106a4f1d7024e90811f084c2ffc506f1de1da" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   def install
